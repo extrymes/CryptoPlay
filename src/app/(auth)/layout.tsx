9cc22684c1b1c944b88cc5d8f066a91be49fc503
extrymes <65893656+extrymes@users.noreply.github.com>
2025-02-27 17:56:37 +0100

    feat(auth): add dynamic logo and footer to auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,34 @@
+"use client";
+
+import Footer from "@/components/footer";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { resolvedTheme } = useTheme();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  });
+
+  if (!isMounted) return null;
   return (
-    <div className="flex min-h-screen items-center justify-center mx-5">
-      {children}
-    </div>
+    <section className="flex flex-col min-h-screen items-center justify-between py-5">
+      <Image
+        src={resolvedTheme === "light" ? "/logo-black.png" : "/logo-white.png"}
+        alt="Logo"
+        width={250}
+        height={250}
+        priority
+      />
+      <main>{children}</main>
+      <Footer />
+    </section>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex flex-col sm:flex-row items-center justify-center text-sm sm:text-base text-gray-500 sm:gap-10">
+      <div>© 2025 CryptoPlay</div>
+    </footer>
+  );
+}
